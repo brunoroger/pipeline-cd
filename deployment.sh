a9da0d38252201d491d4383c14cbe70ebcea83d8
brunoschiochetzup <bschiochet@gmail.com>
2019-07-09 08:38:26 -0300

    push

--- deployment.sh
+++ deployment.sh
@@ -22,7 +22,7 @@ gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
 docker build -t gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1 .
 
-gcloud docker --push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
+gcloud docker -- push gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
 
 kubectl set image deployment/${DEPLOYMENT_NAME} ${CONTAINER_NAME}=gcr.io/${PROJECT_ID}/${REG_ID}:$CIRCLE_SHA1
 
